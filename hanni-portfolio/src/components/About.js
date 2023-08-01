750fc43b51e82875c0e6ab4e90070e97ea1d7aea
BrockHanni <hannibr26@gmail.com>
2023-07-31 21:02:19 -0600

    components, app working

---
+++ hanni-portfolio/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <section id="about">
+                <div className="container">
+                    <h1 className="section-title">Hi I'm Brock, your next Front-End Web Developer</h1>
+                    <p>Heres some stuff about me!</p>
+                    {/* link to resume, and projects, and contact */}
+                    {/* image */}
+                </div>
+            </section>
+        </div>
+    )
+}
+
---
+++ hanni-portfolio/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Phone: 801-647-1286</p>
+            <p>Email:<a href="mailto:hannibr26@gmail.com">Here</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/brock-hanni-803b37215/">Here</a></p>
+            <p>GitHub: <a href="https://github.com/BrockHanni/">Here</a></p>
+        </div>
+    )
+
+}
